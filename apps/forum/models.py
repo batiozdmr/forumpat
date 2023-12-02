751fbf295cbdef23147bbb94032ce0e586a80588
KeremUlker1 <keremito14@gmail.com>
2023-12-02 14:58:22 +0300

    konuyu yaptım

--- apps/forum/models.py
+++ apps/forum/models.py
@@ -1,27 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-class subject (models.Model):
-    text =
+
+class Topic(models.Model):
+    title = models.CharField(max_length=120, blank=False)
+    created_at = models.DateField()
+    is_active = models.BooleanField(default=False)
 
 
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+    userid = models.ForeignKey(User, on_delete=models.PROTECT)
+    stasus = models.BooleanField(default=False)
+    created_at = models.DateField()
+    topic_id = models.ForeignKey(Topic, on_delete=models.CASCADE)
 
 
-
-class question(models.Model):
-
-
-
-
-
-
-
-class answers (models.Model):
-
-
-
-
-
-
-
-
-
+class Answers(models.Model):
+    text = models.CharField()
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.BooleanField(default=False)
+    created_at = models.DateField()
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
