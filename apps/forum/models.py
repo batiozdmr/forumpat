c17b2cb56c26f23a1d96c6dd65d3ab4e36b66b05
isotberatv2 <151997692+isotberatv2@users.noreply.github.com>
2023-12-02 14:40:26 +0300

    Update models.py

--- apps/forum/models.py
+++ apps/forum/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class subject (models.Model):
@@ -7,7 +8,15 @@ class subject (models.Model):
 
 
 
-class question(models.Model):
+
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+    userid = models.ForeignKey(User, on_delete=models.PROTECT)
+    stasus = models.BooleanField(default=False)
+    created_at = models.DateField()
+    #topic_id = models.ForeignKey(topic, on_delete=models.CASCADE)//
 
 
 
