83223bf01cf8afcab359c0a86a3c584c902e7a71
KeremUlker1 <keremito14@gmail.com>
2023-12-02 14:36:28 +0300

    konuyu yaptım

--- apps/forum/models.py
+++ apps/forum/models.py
@@ -1,27 +1,7 @@
 from django.db import models
 
-class subject (models.Model):
-    text =
 
-
-
-
-
-class question(models.Model):
-
-
-
-
-
-
-
-class answers (models.Model):
-
-
-
-
-
-
-
-
-
+class Topic(models.Model):
+    title = models.CharField(max_length=120, blank=False)
+    created_at = models.DateField()
+    is_active = models.BooleanField(default=False)
