dfa5b8884557962a6eb8b397922eac06472bfa65
havin843 <havindrms=@gmail.com>
2023-12-02 15:29:32 +0300

    fix

--- forumpat/urls.py
+++ forumpat/urls.py
@@ -1,7 +1,9 @@
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('apps.forum.urls', namespace='mainpage')),
     path('admin/', admin.site.urls),
     path('accounts/',include("allauth.urls")),
 
